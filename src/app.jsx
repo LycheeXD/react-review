8d7c31dd54c5b628244545c63734b3c8faa76f93
LycheeXD <lycheeexd@gmail.com>
2017-11-14 00:04:12 -0800

    set up react router

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>React Review</p>, document.getElementById('app'));
+const Expenses = () => (
+  <div>
+    test
+  </div>
+);
+
+const Create = () => (
+  <div>
+    route success
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={Expenses} exact />
+        <Route path="/create" component={Create} />
+      </Switch>
+    </div>
+    
+
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
